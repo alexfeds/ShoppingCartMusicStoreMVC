ca374173b30e38f25de0ab54198c2b533643c38a
alexfeds <alexfeds@gmail.com>
2021-04-12 23:52:41 +0100

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoppingCartMusicStore.MvcApplication" Language="C#" %>
---
+++ Models/MusicStoreEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartMusicStore.Models
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public System.Data.Entity.DbSet<ShoppingCartMusicStore.Models.Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public System.Data.Entity.DbSet<ShoppingCartMusicStore.Models.Artist> Artists { get; set; }
+    }
+}
